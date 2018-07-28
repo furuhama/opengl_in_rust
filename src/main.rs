9f8fbaf16ebdc59434aeaf57b906e4f925a15c79
furuhama <furuhama@users.noreply.github.com>
2018-07-28 16:34:37 +0900

    Set attribute & viewport for video_subsystem

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,13 @@ fn main() {
 
     let _gl_context = window.gl_create_context().unwrap();
     let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let gl_attr = video_subsystem.gl_attr();
+
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5); // Use OpenGL Core 4.5
 
     unsafe {
+        gl::Viewport(0, 0, 900, 700);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
